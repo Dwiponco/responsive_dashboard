84ec0e8bd9378f320f6d6170e3e769862a6d205a
Dwi Ponco Utomo <dwiponcoutomo@192.168.100.10>
2022-10-16 22:48:32 +0700

    ecommerce page (done)

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -6,7 +6,7 @@ import { GoPrimitiveDot } from 'react-icons/go';
 import { Stacked, Pie, Button, LineChart, SparkLine } from '../components';
 
 import { useStateContext } from '../contexts/ContextProvider';
-import { earningData } from '../data/dummy';
+import { earningData, SparklineAreaData } from '../data/dummy';
 
 const Ecommerce = () => {
   return (
@@ -30,7 +30,7 @@ const Ecommerce = () => {
           </div>
         </div>
 
-        <div className='flex m-3 justify-center gap-1 items-center'>
+        <div className='flex m-3 flex-wrap justify-center gap-1 items-center'>
           {earningData.map((item) =>(
             <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-xl'>
                 <button 
@@ -40,11 +40,82 @@ const Ecommerce = () => {
                   >
                     {item.icon}
                 </button>
+                <p className='mt-3'>
+                  <span className='text-lg font-semibold'>{item.amount}</span>
+                  <span className={`text-sm text-${item.pcColor} ml-2`}>{item.percentage}</span>
+                </p>
+                <p className='text-sm text-gray-400 mt-1'>{item.title}</p>
             </div>
           ))}
         </div>
 
       </div>
+
+      <div className='flex gap-10 flex-wrap justify-center'>
+        <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780'>
+          <div className='flex justify-between'>
+            <p className='font-semibold text-xl'>Revenue Updates</p>
+            <div className='flex items-center gap-4'>
+              <p className='flex items-center gap-2 text-gray-600 hover:drop-shadow-xl'>
+                <span><GoPrimitiveDot/></span>
+                <span>Expense</span>
+              </p>
+              <p className='flex items-center gap-2 text-green-600 hover:drop-shadow-xl'>
+                <span><GoPrimitiveDot/></span>
+                <span>Budget</span>
+              </p>
+            </div>
+          </div>
+          
+          <div className='mt-10 flex gap-10 flex-wrap justify-center'>
+            {/* REVENUE UPDATES */}
+            <div className='border-r-1 border-color m-4 pr-10'>
+              <div>
+                <p>
+                  <span className='text-3xl font-semibold'>$93,134</span>
+                  <span className='p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs'>25%</span>
+                </p>
+                <p className='text-gray-500 mt-1'>Budget</p>
+              </div>
+              <div className='mt-8'>
+                <p>
+                  <span className='text-3xl font-semibold'>$98,134</span>
+                </p>
+                <p className='text-gray-500 mt-1'>Expense</p>
+              </div>
+              
+              <div className='mt-5'>
+                <SparkLine
+                  currentColor='blue'
+                  id='line-sparkline'
+                  type='Line'
+                  height='80px'
+                  width='250px'
+                  data={SparklineAreaData}
+                  color='blue'
+                />
+              </div>
+              <div className='mt-10'>
+                <Button 
+                  color='white'
+                  bgColor='blue'
+                  text='Download Report'
+                  borderRadius='10px'
+                />
+              </div>
+            </div>
+            {/* BAR CHART */}
+            <div>
+              <Stacked
+                width='320px'
+                height='360px'
+              />
+            </div>
+          </div>
+
+        </div>
+      </div>
+
     </div>
   )
 }
